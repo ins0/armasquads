4e12a597f191d0b05915523d582c361bd5493cc5
ins0 <marco.rieger@aubi-plus.de>
2014-06-05 00:42:39 +0200

    lang fix logout

--- module/Frontend/Login/language/en_EN.php
+++ module/Frontend/Login/language/en_EN.php
@@ -2,7 +2,7 @@
 return array(
 
     'FRONTEND_LOGIN_NAV_LOGIN'          =>  'Login / Sign up',
-    'FRONTEND_LOGIN_NAV_LOGOUT'         =>  'Abmelden',
+    'FRONTEND_LOGIN_NAV_LOGOUT'         =>  'Logout',
     'FRONTEND_LOGIN_TITLE'              =>  'Login / Sign up',
     'FRONTEND_LOGIN_LABEL_LOGIN'        =>  'Login with Account',
     'FRONTEND_LOGIN_LABEL_REGISTER'     =>  'Register new Account',
