0b33ab426722657a9b76bd33c31c7d3768df8f0b
ins0 <marco.rieger@aubi-plus.de>
2014-06-07 02:40:42 +0200

    added stats file cache

--- module/Frontend/Application/config/module.config.php
+++ module/Frontend/Application/config/module.config.php
@@ -1,6 +1,8 @@
 <?php
 namespace Frontend\Application;
 
+use Zend\Cache\StorageFactory;
+
 return array (
 
     'translator' => array (
@@ -14,6 +16,27 @@ return array (
             ),
 
         )
+    ),
+
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Cache\Storage\Filesystem' => function($sm){
+                    $cache = StorageFactory::factory(array(
+                        'adapter' => 'filesystem',
+                        'plugins' => array(
+                            'exception_handler' => array('throw_exceptions' => false),
+                            'serializer'
+                        )
+                    ));
+
+                    $cache->setOptions(array(
+                        'cache_dir' => './data/cache',
+                        'ttl' => 3600,
+                    ));
+
+                    return $cache;
+                },
+        ),
     ),
 
     // ###############################################
