e34725d7c0502bc994bae86e57daf6d4c8d63d3b
ins0 <marco.rieger@aubi-plus.de>
2014-06-11 21:59:23 +0200

    typo + small html fixes

--- module/Frontend/Login/language/de_DE.php
+++ module/Frontend/Login/language/de_DE.php
@@ -7,7 +7,7 @@ return array(
     'FRONTEND_LOGIN_LABEL_LOGIN'        =>  'Anmelden mit Account',
     'FRONTEND_LOGIN_LABEL_REGISTER'     =>  'Neuen Account erstellen',
 
-    'FRONTEND_LOGIN_AUTH_WRONG'     =>  'Benutzer/Passwort falsch!',
+    'FRONTEND_LOGIN_AUTH_WRONG'     =>  'E-Mail/Passwort kombination falsch!',
     'FRONTEND_LOGIN_AUTH_BANNED'    =>  'Dein Account wurde gesperrt!',
     'FRONTEND_LOGIN_AUTH_LOGOUT'    =>  'Du hast dich erfolgreich vom System abgemeldet.',
 
--- module/Frontend/Login/language/en_EN.php
+++ module/Frontend/Login/language/en_EN.php
@@ -7,7 +7,7 @@ return array(
     'FRONTEND_LOGIN_LABEL_LOGIN'        =>  'Login with Account',
     'FRONTEND_LOGIN_LABEL_REGISTER'     =>  'Register new Account',
 
-    'FRONTEND_LOGIN_AUTH_WRONG'     =>  'Username/Password combination wrong!',
+    'FRONTEND_LOGIN_AUTH_WRONG'     =>  'E-Mail/Password combination wrong!',
     'FRONTEND_LOGIN_AUTH_BANNED'    =>  'Your Account was banned!',
     'FRONTEND_LOGIN_AUTH_LOGOUT'    =>  'Successfully logged out from our system',
 
