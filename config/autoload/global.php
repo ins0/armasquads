2024e334a872280115c9265e858f98487550fdb8
Marco Rieger <marco.rieger@aubi-plus.de>
2015-03-19 22:03:52 +0100

    !removed text

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,27 +11,4 @@
  * credentials from accidentally being committed into version control.
  */
 
-return array(
-
-    'doctrine' => array(
-
-        'connection' => array(
-            'orm_default' => array(
-
-                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
-                'params' => array(
-                    'host'     => 'localhost',
-                    'port'     => '3306',
-                    'user'     => 'root',
-                    'password' => '***REMOVED***',
-                    'dbname'   => 'armasquads',
-                    'charset'  => 'utf8',
-                    'driver_options' => array(
-                        \PDO::MYSQL_ATTR_INIT_COMMAND       => 'SET NAMES utf8',
-                    )
-                )
-            )
-        )
-    ),
-
-);
+return [];
