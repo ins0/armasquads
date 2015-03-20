05059b59af1e4d87f5872c4f422c39ddd8678cd5
Marco Rieger <ins0@users.noreply.github.com>
2015-03-20 09:57:05 +0100

    added live db config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,14 +1,21 @@
 <?php
-/**
- * Productive Configuration Override
- *
- * This configuration override file is for overriding environment-specific and
- * security-sensitive configuration information. Copy this file without the
- * .dist extension at the end and populate values as needed.
- *
- * @NOTE: This file is ignored from Git by default with the .gitignore included
- * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
- * credentials from accidentally being committed into version control.
- */
-
-return [];
+return [
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => [
+                    'host'     => '***LIVE_DB_HOST***',
+                    'port'     => '3306',
+                    'user'     => '***LIVE_DB_USERNAME***',
+                    'password' => '***LIVE_DB_PASSWORD***',
+                    'dbname'   => '***LIVE_DB_NAME***',
+                    'charset'  => 'utf8',
+                    'driver_options' => [
+                        \PDO::MYSQL_ATTR_INIT_COMMAND       => 'SET NAMES utf8',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
