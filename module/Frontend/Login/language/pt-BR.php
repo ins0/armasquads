21c98401dc54a7556cccacf079054ed7f88e2e61
multplaneta <THEODEFRANCA@gmail.com>
2015-11-21 01:27:58 -0200

    Added PT-BR
    Added Portuguese-Brazil Language

---
+++ module/Frontend/Login/language/pt-BR.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+
+    'FRONTEND_LOGIN_NAV_LOGIN'          =>  '<span class="fa fa-sign-in"></span> Entrar / Se cadastrar',
+    'FRONTEND_LOGIN_NAV_LOGOUT'         =>  '<span class="fa fa-sign-out"></span> Sair',
+    'FRONTEND_LOGIN_TITLE'              =>  'Entrar / Se cadastrar',
+    'FRONTEND_LOGIN_LABEL_LOGIN'        =>  'Entrar com uma conta',
+    'FRONTEND_LOGIN_LABEL_REGISTER'     =>  'Registar nova conta',
+
+    'FRONTEND_LOGIN_AUTH_WRONG'     =>  'Combinação de e-mail/senha está errada!',
+    'FRONTEND_LOGIN_AUTH_BANNED'    =>  'Sua conta foi banida!',
+    'FRONTEND_LOGIN_AUTH_LOGOUT'    =>  'Você saiu com sucesso do nosso sistema.',
+
+);
