231c292609d3e8d4fa301b586b81e7249d1e98c1
multplaneta <THEODEFRANCA@gmail.com>
2015-11-21 01:24:30 -0200

    Add PT-BR
    Added Portuguese-Brazil Language File

---
+++ module/Frontend/Squads/language/pt_BR.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+
+    'FRONTEND_SQUADS_NAV_LABEL'          =>  '<span class="fa fa-file-code-o"></span> Squads',
+    'FRONTEND_SQUAD_LOGO_INVALID_FORMAT' =>  'Envie uma logo em uma das seguintes dimensões: 16x16/32x32/64x64/128x128/256x256',
+);
