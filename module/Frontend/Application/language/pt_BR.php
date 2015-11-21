d80947c085be43d2bb6cce0c8151bb33e10e3efe
multplaneta <THEODEFRANCA@gmail.com>
2015-11-21 00:59:11 -0200

    PT-BR language
    Created PT-BR language file

---
+++ module/Frontend/Application/language/pt_BR.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+
+    'FRONTEND_NAV_BRAND'                        =>  'ArmAsquads.com - Hospedagem de Squad XML',
+    'FRONTEND_META_DESCRIPTION'                 =>  'Crie e gerencie seus arquivos de Squad do ArmA diretamente online. As logos transparentes são convertidas online para arquivo PAA. Nenhum software ou espaço na web são necessários.',
+
+    'FRONTEND_LAYOUT_GITHUB_HINT'               =>    '<strong>Atenção!</strong> Você encontrou um bug desagradável ou esta faltando uma característica importante?<br />
+                                                       <a href="https://github.com/ins0/armasquads.de/issues" target="_blank"><em><b>Poste em nosso repositório no GitHub!</b></em></a>',
+    'FRONTEND_LAYOUT_DELETE_MODAL'              => 'Uma vez que isto for deletado, não ha volta!',
+    'FRONTEND_LAYOUT_DELETE_MODAL_OK'           => 'Tenho certeza!',
+    'FRONTEND_LAYOUT_DELETE_MODAL_ABORT'        => 'Não',
+
+    'FRONTEND_APPLICATION_NAV_MENUE' =>  '<span class="fa fa-bars"></span> Menu',
+    'FRONTEND_BUY_ME_A_DRING'           =>  'Suporte',
+);
