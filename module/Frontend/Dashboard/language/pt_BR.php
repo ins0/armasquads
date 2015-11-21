0763514581cf01987dc384453a2a1ab796a5e310
multplaneta <THEODEFRANCA@gmail.com>
2015-11-21 01:30:23 -0200

    Added PT-BR
    Added Portuguese-Brazil Language

---
+++ module/Frontend/Dashboard/language/pt_BR.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'FRONTEND_DASHBOARD_NAV_LABEL' => '<span class="fa fa-home"></span> Inicio',
+    'FRONTEND_SUPPORT_NAV_LABEL' => '<span class="fa fa-area-chart"></span> Estatísticas',
+    'FRONTEND_ACCOUNT_NAV_LABEL' => '<span class="fa fa-user"></span> Conta',
+);
