5f408a079eb70d9d730844aefd50b157609117c2
Marco Rieger <marco.rieger@aubi-plus.de>
2015-12-15 00:26:12 +0100

    added 403 template

--- module/Frontend/Application/config/module.config.php
+++ module/Frontend/Application/config/module.config.php
@@ -59,7 +59,7 @@ return array (
 
             // error layouts
             'error/404' => __DIR__ . '/../view/layout/frontend/error/404.phtml',
-            'error/403' => __DIR__ . '/../view/layout/frontend/error/404.phtml',
+            'error/403' => __DIR__ . '/../view/layout/frontend/error/403.phtml',
             'error/index' => __DIR__ . '/../view/layout/frontend/error/exception.phtml',
         ),
         // default layout
